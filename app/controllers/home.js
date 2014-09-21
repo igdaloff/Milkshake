e3ef813c2c918c6c3e2cfd8e1c5fc4f4459e323b
James Shakespeare <j@jshakespeare.com>
2014-09-21 00:10:14 -0400

    cycle through gifs; downsampled some gifs

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,13 +1,9 @@
 exports.home = function(req, res){
 
   // Show the splash page if this is Production
-  if(process.env.NODE_ENV === "production") {
+  if(true) {
 
-    var images = ['kelis', 'sugarlumps', 'thriller', 'gangnam', 'hammer'];
-    var image = images[Math.floor(Math.random() * images.length)];
-    res.render('splash', {
-      image: image
-    });  
+    res.render('splash');  
   }
   else {
     res.render('new-playlist');
