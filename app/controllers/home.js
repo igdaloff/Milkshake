7f20d82416dd7bfbd68985847c4c7eacf80a31a6
James Shakespeare <j@jshakespeare.com>
2014-09-20 23:34:53 -0400

    turn splash page off again for all but prod

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 exports.home = function(req, res){
 
   // Show the splash page if this is Production
-  if(true) {
+  if(process.env.NODE_ENV === "production") {
 
     var images = ['kelis', 'sugarlumps', 'thriller', 'gangnam', 'hammer'];
     var image = images[Math.floor(Math.random() * images.length)];
