e709114fa773571d7f845290c3fbec2c9686f2c7
James Shakespeare <j@jshakespeare.com>
2014-09-20 23:33:44 -0400

    splash page is 9000 times more awesome

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 gem 'sass', '3.4.5'
-gem 'compass', '0.12.2'
+gem 'compass', '1.0.1'
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,13 @@
 exports.home = function(req, res){
 
   // Show the splash page if this is Production
-  if(process.env.NODE_ENV === "production") {
+  if(true) {
 
-    res.render('splash');  
+    var images = ['kelis', 'sugarlumps', 'thriller', 'gangnam', 'hammer'];
+    var image = images[Math.floor(Math.random() * images.length)];
+    res.render('splash', {
+      image: image
+    });  
   }
   else {
     res.render('new-playlist');
