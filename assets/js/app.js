68c4755f28a17c01291f1b5c1a55c5f420a6bc2b
James Shakespeare <j@jshakespeare.com>
2014-10-14 20:01:28 -0400

    put mobile warning in the header so it shows first

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,1 @@
 var TWM = new Marionette.Application();
-
-//Detect if user is on mobile; show different content accordingly
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	$('body').addClass('user-on-mobile');
-}
