19b8f24caf8c179ac61d6cbb25b94dfd416949ef
James Shakespeare <j@jshakespeare.com>
2014-10-17 23:21:50 -0400

    abort existing search if there was one

--- assets/js/apps/playlist/create/trackSearch/trackSearch_controller.js
+++ assets/js/apps/playlist/create/trackSearch/trackSearch_controller.js
@@ -1,13 +1,18 @@
 TWM.module('Playlist.Create.TrackSearch', function(TrackSearch, TWM, Backbone, Marionette, $, _){
 
-  var playlistManager;
+  var playlistManager, currentSearch;
 
   TrackSearch.Controller = {
     searchTracks: function(query) {
 
       var resultsCollection = TWM.request('trackSearch:resultsCollection');
       resultsCollection.query = query;
-      resultsCollection.fetch({
+      // Abort existing search if there is one
+      if(typeof currentSearch !== 'undefined' && currentSearch.abort === 'function') {
+        
+        currentSearch.abort();
+      }
+      currentSearch = resultsCollection.fetch({
         reset: true
       });
       TWM.trigger('trackSearch:newSearch', query);
